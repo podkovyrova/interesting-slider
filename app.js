98631452e27278b924bdb3cd4bad1918dee4f28c
Ekaterina Podkovyrova <86290383+podkovyrova@users.noreply.github.com>
2021-12-09 21:03:34 +0300

    files uploaded

---
+++ app.js
@@ -0,0 +1,38 @@
+const upBtn = document.querySelector('.up-button');
+const downBtn = document.querySelector('.down-button'); //получение кнопок
+const sidebar = document.querySelector('.sidebar');
+const mainSlide = document.querySelector('.main-slide');
+const container = document.querySelector('.container');
+
+const slidesCount = mainSlide.querySelectorAll('div').length; // длина коллекции дивов из контейнера со слайдами
+
+let activeSlideIndex = 0; //какой слайд активный
+
+sidebar.style.top = `-${(slidesCount-1) * 100}vh`;
+
+upBtn.addEventListener('click', () => {
+    changeSlide('up');
+})
+downBtn.addEventListener('click', () => {
+    changeSlide('down');
+})
+
+function changeSlide(direction) {
+    if (direction === 'up') {
+        activeSlideIndex++;
+        if(activeSlideIndex === slidesCount) {
+            activeSlideIndex = 0;
+        }
+    } else if (direction === 'down') {
+        activeSlideIndex--;
+        if(activeSlideIndex < 0) {
+            activeSlideIndex = slidesCount - 1;
+        }
+    }
+
+
+    const heightOfWindow = container.clientHeight;
+
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * heightOfWindow}px)`; //высота окна на индекс активного слайда
+    sidebar.style.transform = `translateY(${activeSlideIndex * heightOfWindow}px)`;
+}
